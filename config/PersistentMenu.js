6ba9b381ec3fa997262286a8116b29c8bc409a14
saimain-primary <saimain.primary@gmail.com>
2023-01-17 22:01:37 +0630

    welcome

--- config/PersistentMenu.js
+++ config/PersistentMenu.js
@@ -1,21 +1,6 @@
 module.exports = [
     {
         type: "postback",
-        title: "🎟️ ကံစမ်းမဲ လက်မှတ် ဝယ်မည်။",
-        payload: "BUY_TICKET",
-    },
-    {
-        type: "postback",
-        title: "🍀 ကံစမ်းမဲ လက်မှတ် စစ်မည်။",
-        payload: "CHECK_TICKET",
-    },
-    {
-        type: "postback",
-        title: "🤝 ကံစမ်းမဲ လက်မှတ် ရောင်းမည်။",
-        payload: "SELL_TICKET",
-    },
-    {
-        type: "postback",
         title: "💬 အကူအညီ ရယူမည်။",
         payload: "GET_RECEIPT",
     },
--- config/WelcomeGeneric.js
+++ config/WelcomeGeneric.js
@@ -1,54 +1,42 @@
 module.exports = [
     {
-        title: "ကံစမ်းမဲ လက်မှတ်",
+        title: "ကံစမ်းမဲ လက်မှတ် ဝယ်မည်",
         image_url:
             "https://www.wooribank.com.kh/wp-content/uploads/2022/03/Promotion-Page-01.png",
-        subtitle: "We have the right hat for everyone.",
-        default_action: {
-            type: "web_url",
-            url: "https://www.originalcoastclothing.com/",
-            webview_height_ratio: "tall",
-        },
+        subtitle:
+            "နှစ်သက်ရာ ကံစမ်းမဲများကို ရွေးချယ်ပြီး ကံစမ်းရန်အတွက် အောက်တွင် နှိပ်ပေးပါဗျ",
         buttons: [
             {
                 type: "postback",
-                title: "ကံစမ်းမဲ လက်မှတ် ဝယ်မည်။",
+                title: "🎟️လက်မှတ် ဝယ်မည်",
                 payload: "BUY_TICKET",
             },
         ],
     },
     {
-        title: "ကံစမ်းမဲ လက်မှတ်",
+        title: "ကံစမ်းမဲ လက်မှတ် စစ်မည်",
         image_url:
             "https://www.wooribank.com.kh/wp-content/uploads/2022/03/Promotion-Page-01.png",
-        subtitle: "We have the right hat for everyone.",
-        default_action: {
-            type: "web_url",
-            url: "https://www.originalcoastclothing.com/",
-            webview_height_ratio: "tall",
-        },
+        subtitle:
+            "သူငယ်ချင်း ကံစမ်းထားတဲ့ လက်မှတ်ကို တိုက်စစ်ဖို့အတွက် အောက်တွင် နှိပ်ပေးပါဗျ",
         buttons: [
             {
                 type: "postback",
-                title: "ကံစမ်းမဲ လက်မှတ် စစ်မည်။",
+                title: "🍀လက်မှတ် စစ်မည်",
                 payload: "CHECK_TICKET",
             },
         ],
     },
     {
-        title: "ကံစမ်းမဲ လက်မှတ်",
+        title: "ကံစမ်းမဲ လက်မှတ် ရောင်းမည်",
         image_url:
             "https://www.wooribank.com.kh/wp-content/uploads/2022/03/Promotion-Page-01.png",
-        subtitle: "We have the right hat for everyone.",
-        default_action: {
-            type: "web_url",
-            url: "https://www.originalcoastclothing.com/",
-            webview_height_ratio: "tall",
-        },
+        subtitle:
+            "သူငယ်ချင်းတို့ ကံစမ်းမဲ ကို ကျွန်တော်တို့နဲ့ ပူးပေါင်းပြီး ရောင်းဖို့အတွက် အောက်တွင် နှိပ်ပေးပါဗျ",
         buttons: [
             {
                 type: "postback",
-                title: "ကံစမ်းမဲ လက်မှတ် ရောင်းမည်။",
+                title: "🤝လက်မှတ် ရောင်းမည်",
                 payload: "SELL_TICKET",
             },
         ],
